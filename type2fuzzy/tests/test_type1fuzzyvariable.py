0ac0ba438edf030819aa9a09f483cca56bde3689
Carmel Gafa <carmelgafa@gmail.com>
2020-03-09 16:30:11 +0100

    version 0.1.40

---
+++ type2fuzzy/tests/test_type1fuzzyvariable.py
@@ -0,0 +1,12 @@
+import unittest
+from type2fuzzy.membership.type1fuzzyvariable import Type1FuzzyVariable
+
+
+class TestType1FuzzyVariable(unittest.TestCase):
+
+    	def test_init(self):
+            var = Type1FuzzyVariable(0, 100 , 100)
+
+            self.assertEqual(var._min_val, 0)
+            self.assertEqual(var._max_val, 100)
+            self.assertEqual(var._res, 100)
